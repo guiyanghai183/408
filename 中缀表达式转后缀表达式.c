92a8270c727aa4b0c2c4bb3625f1ae141e904ffc
guiyanghai183 <52110242+guiyanghai183@users.noreply.github.com>
2023-06-10 16:13:40 +0800

    Add files via upload
    中缀表达式转后缀表达式

---
+++ 中缀表达式转后缀表达式.c
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+stack<char> op;
+stack<int> num;
+
+void eval()
+{
+    auto c = op.top(); op.pop();
+    cout << c << ' ';
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    unordered_map<char, int> pr{{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};
+    for (int i = 0; i < s.size(); i ++ )
+    {
+        if (isdigit(s[i]))
+        {
+            int j = i, x = 0;
+            while (j < s.size() && isdigit(s[j]))
+                x = x * 10 + s[j ++ ] - '0';
+            cout << x << ' ';
+            i = j - 1;
+        }
+        else if (s[i] == '(') op.push(s[i]);
+        else if (s[i] == ')')
+        {
+            while (op.top() != '(') eval();
+            op.pop();
+        }
+        else
+        {
+            while (op.size() && op.top() != '(' && pr[op.top()] >= pr[s[i]])
+                eval();
+            op.push(s[i]);
+        }
+    }
+
+    while (op.size()) eval();
+    
+
+    return 0;
+}
+
