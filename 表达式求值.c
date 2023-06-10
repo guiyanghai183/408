ba1783cf0f0973725ec59469dcc82e75699468ad
guiyanghai183 <52110242+guiyanghai183@users.noreply.github.com>
2023-06-10 16:09:28 +0800

    Add files via upload
    栈的应用

---
+++ 表达式求值.c
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+stack <char> op;
+stack <int> num;
+
+void eval()
+{
+    auto b = num.top(); num.pop();
+    auto a = num.top(); num.pop();
+    auto c = op.top(); op.pop();
+    
+    if(c == '+') num.push(a + b);
+    else if(c == '-') num.push(a - b);
+    else if(c == '*') num.push(a * b);
+    else num.push(a / b);
+}
+
+
+int main()
+{
+    string s;
+    cin >> s;
+    
+    unordered_map <char, int> pr{{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};
+    
+    for(int i = 0; i < s.size(); i ++ )
+    {
+        if(isdigit(s[i]))
+        {
+            int j = i, x = 0;
+            while(isdigit(s[j]) && j < s.size())
+                x = x * 10 + s[j ++ ] - '0';
+            num.push(x);
+            i = j - 1;
+        }
+        else if(s[i] == '(') op.push(s[i]);
+        else if(s[i] == ')')
+        {
+            while(op.top() !='(') eval();
+            op.pop();
+        }
+        else
+        {
+            while(op.size() && op.top() != '(' && pr[op.top()] >= pr[s[i]]) eval();
+            op.push(s[i]);
+        }
+    }
+    
+    while(op.size()) eval();
+    cout << num.top() << endl;
+    
+    return 0;
+}
