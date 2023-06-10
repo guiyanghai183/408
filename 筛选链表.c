7ab64911c430f33273d5dba781f866463ba5963e
guiyanghai183 <52110242+guiyanghai183@users.noreply.github.com>
2023-06-10 16:54:09 +0800

    Update 筛选链表.c
    2015-41

--- 筛选链表.c
+++ 筛选链表.c
@@ -6,6 +6,11 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/*代码逻辑
+开一个bool数组记录链表中每一个节点是否出现过
+需记录判断是否需要删除节点的上一个节点,因为链表的结构
+所以遍历时判断的是下一个节点是否需要删除,第一个节点需要特判
+*/
 class Solution {
 public:
     ListNode* filterList(ListNode* head) {
